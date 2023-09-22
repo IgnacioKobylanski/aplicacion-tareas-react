0e3f26ea57fb06d761469fabaaaf0270fcab15e9
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2023-09-21 21:52:58 -0300

    se creo el logo de la app y se le dio estilo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
+import logo from './imagenes/tareas-logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      React
+      <div className='titulo-contenedor'>
+        <img
+        src={logo}
+        className='tareas-logo'
+        alt='logo'
+        />
+      </div>
     </div>
   );
 }
