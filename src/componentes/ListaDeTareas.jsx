d4a949fe3f15b040747a0c165aab03e0c3710db7
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2023-10-26 20:06:11 -0300

    eliminarTarea is now working and have style

--- src/componentes/ListaDeTareas.jsx
+++ src/componentes/ListaDeTareas.jsx
@@ -7,11 +7,17 @@ function ListaDeTareas(){
   const [tareas, setTareas] = useState([]);
 
   const agregarTarea = tarea => {
+    console.log(tarea);
     if (tarea.texto.trim()) {
       tarea.texto = tarea.texto.trim();
       const tareasActualizadas = [tarea, ...tareas];
       setTareas(tareasActualizadas);
     }
+  }
+
+  const eliminarTarea = id => {
+    const tareasActualizadas = tareas.filter(tarea => tarea.id !==id);
+    setTareas(tareasActualizadas);
   }
 
   return(
@@ -20,8 +26,12 @@ function ListaDeTareas(){
       <div className="tareas-lista-contenedor">
         {
           tareas.map((tarea) =>
-          <Tarea texto={tarea.texto}
-          completada={tarea.completada}/>)
+          <Tarea 
+          key={tarea.id}
+          id={tarea.id}
+          texto={tarea.texto}
+          completada={tarea.completada}
+          eliminarTarea={eliminarTarea}/>)
         }
       </div>
     </>
